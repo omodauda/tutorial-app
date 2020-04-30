c186348634583a6c7b653532b557d81357c889fe
Dauda Lawal <omodauda.dl@gmail.com>
2020-04-30 17:15:34 +0100

    user signup, login and authentication

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const authRoutes = require("./routes/route");
 
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(authRoutes);
 
-//Routes for API
-const router = express.Router();
-
-//test route to make sure it is working (accessed at GET http://localhost:3000/tutorial-app/api/v1)
-router.get('/', function(req,res){
-    res.json({message: "Welcome to tutorial app"});
-});
-
-
-//Register our routes
-app.use('/tutorial-app/api/v1', router);
 
 
 
 app.listen(3000, function(){
     console.log("server running on port 3000!")
-})
+});
+
+mongoose
+    .connect(
+        "mongodb+srv://admin-babslaw:babalola1996@cluster0-hthp7.mongodb.net/tutorialDB",
+        {useNewUrlParser: true, useUnifiedTopology: true}
+    )
+    .then (result => {
+        console.log("Database connected!")
+    })
+    .catch (err => {
+        console.log(err)
+    });
---
+++ controllers/users.js
@@ -0,0 +1,83 @@
+const User = require('../models/users');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signUp = (req, res, next) => {
+    const firstName = req.body.firstName;
+    const email = req.body.email;
+    const password = req.body.password;
+    const role = req.body.role;
+
+    if (!firstName || !email || !password) {
+        res.status(400).send({
+            status: false,
+            message: "All fields are required"
+        })
+        return;
+    }
+
+    User.findOne({email})
+    .then(user => {
+        if (user){
+            return res
+            .status(423)
+            .send({status: false, message: "This email already exists"});
+        }
+    });
+
+    if (role === "admin"){
+        res.status(423).send({
+            status: false,
+            message: "You can't signup as an admin"
+        })
+        return;
+    }
+
+    bcrypt
+    .hash(password, 12)
+    .then(password => {
+      let user = new User({
+        firstName,
+        email,
+        password,
+        role: role
+      });
+      return user.save();
+    })
+    .then(() => res.status(200).send({ status: true, message: "User registered successfully" }))
+    .catch(err => console.log(err));
+};
+
+
+exports.logIn = (req, res, next) => {
+    const firstName = req.body.firstName;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+    .then(user => {
+        if (!user) {
+            return res
+            .status(404)
+            .send("User not found, please provide valid credentials");
+        }
+
+        bcrypt.compare(password, user.password).then(valid => {
+            if (!valid){
+                return res
+                .status(403)
+                .send("Incorrect username or password, please review details and try again");
+            }
+            const token = jwt.sign(
+                { email: user.email, _id: user.id },
+                "somesecretkey",
+                { expiresIn: "3hrs" }
+            );
+            res.status(200).send({
+                _id: user._id,
+                token
+            });
+        });
+    })
+    .catch(err => console.log(err));
+}
